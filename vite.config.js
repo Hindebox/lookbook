f68530d0f8a185178034eb1f487cde5596739675
Hindebox <125393629+Hindebox@users.noreply.github.com>
2023-10-22 13:32:26 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+import react from "@vitejs/plugin-react";
+
+dotenv.config({ path: ".env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.REACT_APP_API_URL": JSON.stringify(
+      process.env.REACT_APP_API_URL
+    ),
+  },
+});
