9cbc13c8e6241fc9138d24b193bb442fc36d00ec
Hindebox <125393629+Hindebox@users.noreply.github.com>
2023-10-24 11:25:08 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { ReactDOM } from "react";
+import { ReactDOM } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Auth from "./pages/Auth.jsx";
