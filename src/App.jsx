cdca60b436051ff5ec2359934ce5d2eba9ffead3
Hindebox <125393629+Hindebox@users.noreply.github.com>
2023-10-24 11:45:36 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,32 @@ import "./assets/style/global.scss";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+import MyAccount from "./pages/MyAccount";
+MyAccount;
 import useAuth from "./hooks/useAuth.js";
 
 function App() {
   const location = useLocation();
-  const isAuthenticated = useAuth();
 
-  return <Home />;
+  //if user not logged in
+  useAuth();
+
+  return (
+    <div>
+      {location.pathname !== "/auth" && <Navbar />}
+      <Routes>
+        <Route path="/" element={location.pathname !== "/auth" && <Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/my-account" element={<MyAccount />} />
+      </Routes>
+      {location.pathname !== "/auth" && <Footer />}
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
-import Auth from "./pages/Auth.jsx";
-import Products from "./pages/Products.jsx";
-import Orders from "./pages/Orders.jsx";
-import MyAccount from "./pages/MyAccount.jsx";
-import NotFound from "./pages/NotFound.jsx";
 
-const Root = () => (
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/my-account" element={<MyAccount />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <App />
+  </Router>
 );
-
-ReactDOM.render(<Root />, document.getElementById("root"));
